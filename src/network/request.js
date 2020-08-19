28eb619b55e5faaa32e7e46eb8dcbae8ebac9451
l1106842466 <1106842466@qq/com>
2020-08-19 14:38:43 +0800

    'change'

---
+++ src/network/request.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+export function request(config) {
+
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  })
+
+  // instance.interceptors.requset.use(config => {
+  //   return config
+  // },err => {
+  //
+  // })
+
+  instance.interceptors.request.use(config => {
+    return config
+  },error => {
+
+  })
+  instance.interceptors.response.use(res => {
+    return res.data
+  },err => {
+
+  })
+
+  return instance(config)
+}
